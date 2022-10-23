265e1bf2073ca19b73bac888a05c2c1d572727b4
96sunghyun <visioner2168@gmail.com>
2022-10-24 01:54:03 +0900

    cpp function-inline

---
+++ 함수/function-inline/function-inline.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+// inline int square(int x) {
+//     return x * x;
+// }
+int main() {
+    // int x = square(10);
+
+    // 위와같이 inline으로 함수를 선언해주면, 내부적으로 함수로 이동하는 것이 아니라
+    // int x = 10 * 10; // 과 같이 바로 내부 로직을 실행하게 된다. -> 함수실행에 필요한 비용이 절약된다.
+    // cout << x << endl;
+}
